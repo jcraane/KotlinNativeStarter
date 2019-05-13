dca12d56e1edbadf6c5d9525f863c8b87575f7fd
jamiecraane <jcraane@gmail.com>
2019-05-13 09:37:37 +0200

    build.gradle -> build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,15 +1,18 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
-    ext.kotlin_version = '1.3.31'
+    /*ext {
+        kotlin_version = '1.3.31'   
+    }*/
     repositories {
         google()
         jcenter()
         
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:3.4.0'
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
+        classpath("com.android.tools.build:gradle:3.4.0")
+        classpath(kotlin("gradle-plugin", version = "1.3.31"))
+//        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
@@ -23,6 +26,8 @@ allprojects {
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks {
+    val clean by registering(Delete::class) {
+        delete(buildDir)
+    }
 }
