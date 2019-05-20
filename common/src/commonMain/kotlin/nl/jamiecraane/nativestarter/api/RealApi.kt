17f3a1fd6d15606ad775842cd80265914567599a
jamiecraane <jcraane@gmail.com>
2019-05-20 20:49:15 +0200

    Add Kto features
    Ktor logging feature.
    Experiment (but commented-out) Ktor JsonFeature. Does not work well
    with endpoints which return a List of objects.

--- common/src/commonMain/kotlin/nl/jamiecraane/nativestarter/api/RealApi.kt
+++ common/src/commonMain/kotlin/nl/jamiecraane/nativestarter/api/RealApi.kt
@@ -1,6 +1,10 @@
 package nl.jamiecraane.nativestarter.api
 
 import io.ktor.client.HttpClient
+import io.ktor.client.features.logging.LogLevel
+import io.ktor.client.features.logging.Logger
+import io.ktor.client.features.logging.Logging
+import io.ktor.client.features.logging.SIMPLE
 import io.ktor.client.request.get
 import io.ktor.client.request.url
 import io.ktor.client.response.HttpResponse
@@ -13,7 +17,19 @@ import kotlinx.serialization.list
 import nl.jamiecraane.nativestarter.domain.Person
 
 class RealApi : Api {
-    private val client = HttpClient() /* {
+    private val client = HttpClient() {
+        install(Logging) {
+            logger = Logger.SIMPLE
+            level = LogLevel.ALL
+        }
+//        install(JsonFeature) {
+//            jsonfeature does not work nicely yet with list, following error:  Fail to run receive pipeline: kotlinx.serialization.SerializationException: Can't locate argument-less serializer for class kotlin.collections.List. For generic classes, such as lists, please provide serializer explicitly.
+//            serializer = KotlinxSerializer(Json.nonstrict)
+//        }
+    }
+
+
+    /* {
        install(Logging) {
             logger = Logger.SIMPLE
             level = LogLevel.ALL
@@ -22,12 +38,12 @@ class RealApi : Api {
 
     override suspend fun retrievePersons(): ApiResponse<List<Person>> {
         val response = client.get<HttpResponse> {
-            url(Url("http://10.128.238.209:2500/persons"))
+            url(Url("http://192.168.1.48:2500/persons"))
         }
 
         return if (response.status.isSuccess()) {
             Success(
-                Json.parse(
+                Json.nonstrict.parse(
                     Person.serializer().list,
                     response.readText(Charset.forName("UTF-8"))
                 )
