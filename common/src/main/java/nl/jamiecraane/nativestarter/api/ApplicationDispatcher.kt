18e58b8986fa987ae380a0a00587f44470790796
jamiecraane <jcraane@gmail.com>
2019-12-09 07:27:45 +0100

    Assign Dispatchers.IO to applicationDispatcher

--- common/src/main/java/nl/jamiecraane/nativestarter/api/ApplicationDispatcher.kt
+++ common/src/main/java/nl/jamiecraane/nativestarter/api/ApplicationDispatcher.kt
@@ -1,6 +1,6 @@
 package nl.jamiecraane.nativestarter.api
 
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 
-internal actual val ApplicationDispatcher: CoroutineDispatcher
-    get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+internal actual val ApplicationDispatcher: CoroutineDispatcher = Dispatchers.IO
